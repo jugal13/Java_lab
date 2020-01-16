c0eee54bf14d8a498764002587ae6e6629772065
Jugal <jugaldeepak@gmail.com>
2020-01-17 03:54:19 +0530

    updated weekly labs

--- Week_Labs/week 1/Fibonacci.java
+++ Week_Labs/week 1/Fibonacci.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
-public class Fibonacci {     
-    @SuppressWarnings("resource")
-	public static void main (String args[]) {
-    	Scanner in = new Scanner(System.in);
-    	int n = in.nextInt();
-    	displayFibonacci(n);
+public class Fibonacci {
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    int n = in .nextInt();
+    displayFibonacci(n);
+  }
+
+  static void displayFibonacci(int n) {
+    int f = 0, s = 1, t;
+    for (int i = 0; i<n; i++) {
+      System.out.print(f + " ");
+      t = f + s;
+      f = s;
+      s = t;
     }
-    
-    static void displayFibonacci(int n) {
-    	int f = 0,s = 1,t;
-    	for (int i = 0; i < n; i++) {
-    		System.out.print(f+" ");
-    		t = f + s;
-    		f = s;
-    		s = t;
-    	}
-    }
+  }
 }
